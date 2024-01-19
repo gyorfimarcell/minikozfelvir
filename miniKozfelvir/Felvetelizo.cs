0ef18dce3b473fed49e75ec960ac41022c1d8799
Györfi Marcell <44555742+gyorfimarcell@users.noreply.github.com>
2024-01-19 11:05:50 +0100

    Add files via upload

---
+++ miniKozfelvir/Felvetelizo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace miniKozfelvir
+{
+    public class Felvetelizo: IFelvetelizo
+    {
+        public const string CSVFEJ = "om_azonosito;nev;email;szuletesi_datum;ertesitesi_cim;matek_eredmeny;magyar_eredmeny";
+
+        string omAzon;
+        string nev;
+        string email;
+        DateTime szuletesiDatum;
+        string ertesitesiCim;
+        byte? matek;
+        byte? magyar;
+
+        public Felvetelizo(string omAzon, string nev, string email, DateTime szuletesiDatum, string ertesitesiCim, byte? matek, byte? magyar)
+        {
+            this.omAzon = omAzon;
+            this.nev = nev;
+            this.email = email;
+            this.szuletesiDatum = szuletesiDatum;
+            this.ertesitesiCim = ertesitesiCim;
+            this.matek = matek;
+            this.magyar = magyar;
+        }
+
+        public Felvetelizo(string csvSor)
+        {
+            ModositCSVSorral(csvSor);
+        }
+
+        public string OM_Azonosito { get => omAzon; set => throw new NotImplementedException(); }
+        public string Neve { get => nev; set => throw new NotImplementedException(); }
+        public string ErtesitesiCime { get => ertesitesiCim; set => throw new NotImplementedException(); }
+        public string Email { get => email; set => throw new NotImplementedException(); }
+        public DateTime SzuletesiDatum { get => szuletesiDatum; set => throw new NotImplementedException(); }
+        public int Matematika { get => Convert.ToInt32(matek); set => throw new NotImplementedException(); }
+        public int Magyar { get => Convert.ToInt32(magyar); set => throw new NotImplementedException(); }
+
+        public override string ToString()
+        {
+            return $"{omAzon};{nev};{email};{szuletesiDatum};{ertesitesiCim};{matek};{magyar}";
+        }
+
+        public string CSVSortAdVissza()
+        {
+            return ToString();
+        }
+
+        public void ModositCSVSorral(string csvString)
+        {
+            string[] mezok = csvString.Split(';');
+
+            this.omAzon = mezok[0];
+            this.nev = mezok[1];
+            this.email = mezok[2];
+            this.szuletesiDatum = DateTime.Parse(mezok[3]);
+            this.ertesitesiCim = mezok[4];
+
+            byte _matek;
+            this.matek = Byte.TryParse(mezok[5], out _matek) ? (byte?)_matek : null;
+
+            byte _magyar;
+            this.magyar = Byte.TryParse(mezok[6], out _magyar) ? (byte?)_magyar : null;
+        }
+    }
+}
---
+++ miniKozfelvir/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+
+namespace miniKozfelvir
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Felvetelizo> felvetelizok;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            felvetelizok = new ObservableCollection<Felvetelizo>(File.ReadAllLines("felvetelizok.csv").Skip(1).Select(x => new Felvetelizo(x)));
+            dgFelvetelizok.ItemsSource = felvetelizok;
+        }
+
+        public void Import() {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.DefaultExt = ".csv";
+            if (ofd.ShowDialog() == true) {
+                List<string> azonositok = felvetelizok.Select(x => x.OM_Azonosito).ToList();
+                File.ReadAllLines(ofd.FileName).Skip(1).Select(x => new Felvetelizo(x)).ToList().ForEach(x => {
+                    if (azonositok.Contains(x.OM_Azonosito)) {
+                        
+                    }
+                    felvetelizok.Add(x);
+                   });
+            }
+        }
+
+        public void Export()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = ".csv";
+            if (sfd.ShowDialog() == true)
+            {
+                File.WriteAllLines(sfd.FileName, felvetelizok.Select(x => x.ToString()).Prepend(Felvetelizo.CSVFEJ).ToList());
+            }
+        }
+
+        private void btnTorol_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgFelvetelizok.SelectedIndex != -1) {
+                felvetelizok.RemoveAt(dgFelvetelizok.SelectedIndex);
+            }
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            Export();
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            Import();
+        }
+    }
+}
